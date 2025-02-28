d383a260f9afee9e34784f7b27a8a897dd476fb4
nagesh <chintadanagesh9@gmail.com>
2025-02-28 10:29:39 +0000

    consolemsgchanged

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -79,7 +79,7 @@ const Leaderboard = () => {
   const renderFailureView = () => {
      const {errorMsg} = apiResponse 
      return <ErrorMessage>{errorMsg}</ErrorMessage>
-     console.log(errorMsg)
+     console.log("Welocome to React Hooks")
   }
 
   const renderLeaderboard = () => {
